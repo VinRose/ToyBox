2ea135896ad0a0283219eb83a518ec96df03db3c
Desktop <k1994pino@gmail.com>
2018-12-15 18:49:17 +0900

    feat: try to consider alternative proposal of enum inheritance

---
+++ EnumInheritance/EnumInheritance/Container.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumInheritance
+{
+    public class Container
+    {
+        /// <summary>
+        /// enum which is succeding Food
+        /// </summary>
+        public enum Fish
+        {
+            Tuna = Food.Tuna,
+            Salmon = Food.Salmon
+        }
+
+        /// <summary>
+        /// enum which is succeding Food
+        /// </summary>
+        public enum Fruit
+        {
+            Orange = Food.Orange,
+            Apple = Food.Apple
+        }
+
+        /// <summary>
+        /// based enum
+        /// </summary>
+        public enum Food
+        {
+            Tuna,
+            Salmon,
+            Orange,
+            Apple
+        }
+
+        public static string ParseStuff(string input)
+        {
+            return Enum.IsDefined(typeof(Food), input) ?
+                Enum.Parse(typeof(Food), input).ToString() : "error";
+        }
+    }
+}
---
+++ EnumInheritance/EnumInheritance/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string meat = "Beef";
+            Console.WriteLine(Container.ParseStuff(meat));
+            string fish = "Tuna";
+            Console.WriteLine(Container.ParseStuff(fish));
+            string fruit = "Apple";
+            Console.WriteLine(Container.ParseStuff(fruit));
+        }
+    }
+}
